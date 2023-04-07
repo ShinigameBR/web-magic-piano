eb3e46bd987f57f75c9d5a33d74d8c7bd9ba036a
ShinigameBR <shinigamecontato@gmail.com>
2023-04-07 11:19:55 -0300

    Add pressed effect

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll('.key');
 
+
+
 function playSound(note) {
   var audio = new Audio(`sounds/key${note}.mp3`);
   audio.play();
@@ -8,6 +10,17 @@ function playSound(note) {
 buttons.forEach(button => {
   button.addEventListener('click', function() {
     playSound(button.dataset.note);
+    if (button.classList.contains('white')) {
+      button.style.backgroundColor = '#aaa'
+      setTimeout(function() {
+        button.style.backgroundColor = '#fff'
+      }, 100)
+    }else if (button.classList.contains('black')) {
+      button.style.backgroundColor = '#333';
+      setTimeout(function() {
+        button.style.backgroundColor = '#222';
+      }, 100)
+    } 
   });
 });
 
@@ -15,6 +28,17 @@ document.addEventListener('keydown', function(event) {
   buttons.forEach(button => {
     if (event.key === button.querySelector('span').innerText) {
       playSound(button.dataset.note);
+      if (button.classList.contains('white')) {
+        button.style.backgroundColor = '#aaa'
+        setTimeout(function() {
+          button.style.backgroundColor = '#fff'
+        }, 100)
+      }else if (button.classList.contains('black')) {
+        button.style.backgroundColor = '#333';
+        setTimeout(function() {
+          button.style.backgroundColor = '#222';
+        }, 100)
+      }   
     }
   });
 });
